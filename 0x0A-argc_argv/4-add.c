f9eba3658bceb3e7a75f7b84e0737ac9077efc3a
adebayodeji <adebayodeji14@yahoo.com>
2023-04-10 20:08:08 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,58 +1,37 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#define UNUSED(x) (void)(x)
-/**
- * StringCheck - checks string
- * @s: string to check
- * Return: boolean
- */
-int StringCheck(char *s)
-{
-	int i = 0;
 
-	for (; s[i] != '\0'; i++)
-	{
-		if (!isdigit(s[i]))
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
 /**
- * main - main function
- * @argc: argumentc
- * @argv: vector of arguments
- *Return: always 0
+ * main - add 2 positive numbers and print the result
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Description: If no number is passed to program, print 0.
+ * If one of the numbers contain non-digits, print Error.
+ * Return: 1 if error, 0 if function runs properly.
  */
-int main(int argc, char  *argv[])
-{
-	int i;
-	int result = 0;
 
+int main(int argc, char *argv[])
+{
+	int total, i;
+	char *p;
+	int num;
+
+	total = 0;
 	if (argc > 1)
 	{
-		for (i = 1; i < argc; i++)
+		for (i = 1; argv[i]; i++)
 		{
-			if (StringCheck(argv[i]))
-			{
-				result += atoi(argv[i]);
-			}
+			num = strtol(argv[i], &p, 10);
+			if (!*p)
+				total += num;
 			else
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		printf("%d\n", result);
-		return (0);
 	}
-	else
-	{
-		printf("%d\n", 0);
-		return (1);
-	}
-
+	printf("%d\n", total);
+	return (0);
 }
